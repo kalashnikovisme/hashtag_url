a85d18dbe5c364608d11faac173488135f3f3c31
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-06-01 01:04:11 +0400

    Some updates

--- lib/hashtag_url.rb
+++ lib/hashtag_url.rb
@@ -3,7 +3,7 @@ require "hashtag_url/version"
 module HashtagUrl
   SUPPORTED_NETWORKS = [ 'vk', 'vkontakte', 'twitter', 'instagram', 'facebook', 'fb' ]
   def hashtag_url(hashtag, social_network)
-    if SUPPORTED_NETWORKS.include? social_network
+    if SUPPORTED_NETWORKS.include? social_network.to_s.underscore
       send social_network, hashtag.to_s.gsub('#', '')
     else
       raise "Gem HashtagUrl doesn't support #{social_network} hashtag links. You can update gem on http://github.com/kalashnikovisme/hashtag_url It's easy!"
