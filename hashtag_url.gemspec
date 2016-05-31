9ce77d9368a0bf89e4e23540a89c842eb9947232
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-06-01 00:58:06 +0400

    Fix functionatl

--- hashtag_url.gemspec
+++ hashtag_url.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Pavel Kalashnikov"]
   spec.email         = ["kalashnikovisme@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Easy urls on social networks hash tags }
+  spec.description   = %q{Easy urls on social networks hash tags }
+  spec.homepage      = "https://github.com/kalashnikovisme/hashtag_url"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
--- lib/hashtag_url.rb
+++ lib/hashtag_url.rb
@@ -1,11 +1,10 @@
 require "hashtag_url/version"
 
 module HashtagUrl
-  SUPPORTED_NETWORKS = [ 'vk', 'vkontakte', 'twitter', 'instagram', 'facebook' 'fb' ]
+  SUPPORTED_NETWORKS = [ 'vk', 'vkontakte', 'twitter', 'instagram', 'facebook', 'fb' ]
   def hashtag_url(hashtag, social_network)
-    ht = hashtag.to_s.gsub('#', '')
-    if SUPPORTED_NETWORKS.include? ht
-      send social_network, ht
+    if SUPPORTED_NETWORKS.include? social_network
+      send social_network, hashtag.to_s.gsub('#', '')
     else
       raise "Gem HashtagUrl doesn't support #{social_network} hashtag links. You can update gem on http://github.com/kalashnikovisme/hashtag_url It's easy!"
     end
@@ -13,22 +12,22 @@ module HashtagUrl
 
   private
 
-  def vk
-    "vk.com/feed?q=##{hashtag}&section=search"
+  def vk(hashtag)
+    "http://vk.com/feed?q=%23#{hashtag}&section=search"
   end
 
   alias vkontakte vk
 
-  def twitter
-    "twitter.com/hashtag/#{hashtag}?src=hash"
+  def twitter(hashtag)
+    "http://twitter.com/hashtag/#{hashtag}?src=hash"
   end
 
-  def instagram
-    "instagram.com/explore/tags/#{hashtag}"
+  def instagram(hashtag)
+    "http://instagram.com/explore/tags/#{hashtag}"
   end
   
-  def facebook
-    "facebook.com/hashtag/#{hashtag}?source=feed_text"
+  def facebook(hashtag)
+    "http://facebook.com/hashtag/#{hashtag}?source=feed_text"
   end
 
   alias fb facebook
